846ecb3d408281886b2e315f38ecb7bb9044247b
JasonMakana <Jasonzaga1985@gmail.com>
2026-04-17 20:31:45 -0600

    creacion de las migraciones - vacias

---
+++ app/Http/Controllers/DetallesOrdenController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\detalles_orden;
+use Illuminate\Http\Request;
+
+class DetallesOrdenController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(detalles_orden $detalles_orden)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(detalles_orden $detalles_orden)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, detalles_orden $detalles_orden)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(detalles_orden $detalles_orden)
+    {
+        //
+    }
+}

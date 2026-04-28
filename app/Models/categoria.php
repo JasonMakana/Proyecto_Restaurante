ae5532a1a2428a97a4cf3feb57021c5aefb59c3b
JasonMakana <Jasonzaga1985@gmail.com>
2026-04-27 22:36:08 -0600

    Relacion entre tablas

--- app/Models/categoria.php
+++ app/Models/categoria.php
@@ -4,7 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class categoria extends Model
+class Categoria extends Model
 {
-    //
+    // Asignamos el nombre de la tabla a la clase modelo
+    protected $table = 'categorias';
+
+    public function platillos(){
+        return $this->hasMany(Platillo::class, 'categoria_id');
+    }
+
+    // Solo se permitira agregar la descripcion de la categoria, el id se asignara 
+    // automaticamente al momento de crear la categoria
+    protected $fillable = [
+        'categoria',
+        'descripcion'
+    ];
 }
--- app/Models/orden.php
+++ app/Models/orden.php
@@ -4,7 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class orden extends Model
+class Orden extends Model
 {
-    //
+    // Asignamos el nombre de la tabla a la clase modelo
+    protected $table = 'ordenes';
+
+    public function user(){
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function detallesOrden(){
+        return $this->hasMany(DetalleOrden::class, 'orden_id');
+    }
+
+    protected $fillable = [
+        'estado'
+    ];
 }
